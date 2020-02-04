91e3fe308aec50b1b76db5508aab7021ff8a31b8
sumit kumar <sumit1523@gmail.com>
2020-02-05 01:50:42 +0530

    Added Dashboard and notFound page

---
+++ src/modules/Authentication/components/Login/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Dashboard = () => {
+    return (
+        <article>
+            <section className='text-section'>
+                <h1>User Dashboard</h1>
+                <p>
+                    Welcome, you are logged in!
+                </p>
+            </section>
+        </article>
+    )
+}
+
+export default Dashboard
---
+++ src/modules/Authentication/components/Login/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <article>
+            <h1>Page not found.</h1>
+        </article>
+    )
+}
+
+export default NotFound
