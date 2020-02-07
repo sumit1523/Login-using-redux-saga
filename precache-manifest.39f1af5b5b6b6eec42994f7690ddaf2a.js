9d39457a96ed32baa49197dd85e7a51a9295c8a6
sumit kumar <sumit.u.kumar@capgemini.com>
2020-02-07 14:33:06 +0530

    Updates

---
+++ precache-manifest.39f1af5b5b6b6eec42994f7690ddaf2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d50e184b511278d1dd5081d6e74efa84",
+    "url": "/Login-using-redux-saga/index.html"
+  },
+  {
+    "revision": "76996721c344a38f576b",
+    "url": "/Login-using-redux-saga/static/css/main.9a3135b6.chunk.css"
+  },
+  {
+    "revision": "73b56464dee058fa9e2d",
+    "url": "/Login-using-redux-saga/static/js/2.b8846d0c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Login-using-redux-saga/static/js/2.b8846d0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76996721c344a38f576b",
+    "url": "/Login-using-redux-saga/static/js/main.f47539f7.chunk.js"
+  },
+  {
+    "revision": "4ece140f290ce82fc82f",
+    "url": "/Login-using-redux-saga/static/js/runtime-main.79a2f616.js"
+  }
+]);
